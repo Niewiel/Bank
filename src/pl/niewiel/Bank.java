ecab1c0cb6b9a0b29eff36ad0820113354552cc2
tniewiel <tniewiel@gmail.com>
2018-01-12 19:36:10 +0100

    initial

--- src/pl/niewiel/Bank.java
+++ src/pl/niewiel/Bank.java
@@ -1,64 +1,44 @@
 package pl.niewiel;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
+
+import org.jetbrains.annotations.Contract;
 
 class Bank {
     static final int LICZBA_KONT = 100;
     static final int SALDO_POCZATKOWE = 100;
     private int[] konta;
-    private ReentrantLock bankLock;
-    private Condition wystarczajaceSrodki;
 
     Bank() {
         konta = new int[LICZBA_KONT];
         for (int i = 0; i < LICZBA_KONT; i++) {
             konta[i] = SALDO_POCZATKOWE;
         }
-        bankLock = new ReentrantLock();
-        wystarczajaceSrodki=bankLock.newCondition();
     }
 
-    void przelew(int z, int na, int kwota) throws InterruptedException {
-        bankLock.lock();
-        try {
-            while (konta[z]<kwota){
-                wystarczajaceSrodki.await();
-            }
-            assert konta[z]>=kwota:"Saldo= "+getSaldo(z)+" kwota= "+kwota;
-            System.out.println(Thread.currentThread());
-            konta[z] -= kwota;
-            System.out.println("\nz: " + z + "\nna: " + na + "\nkwota: " + kwota);
-            konta[na] += kwota;
-            System.out.println("Saldo ogolne: " + getSaldoOgolne());
-            wystarczajaceSrodki.signalAll();
-        } finally {
-            bankLock.unlock();
-        }
+    synchronized void przelew(int z, int na, int kwota) throws InterruptedException{
+        while (kwota>getSaldo(z))
+            wait();
+        assert konta[z] >= kwota : "Saldo= " + getSaldo(z) + " kwota= " + kwota;
+        System.out.println(Thread.currentThread());
+        konta[z] -= kwota;
+        System.out.println("\nz: " + z + "\nna: " + na + "\nkwota: " + kwota);
+        konta[na] += kwota;
+        System.out.println("Saldo ogolne: " + getSaldoOgolne());
+        notifyAll();
     }
 
-    private int getSaldoOgolne() {
-
+    @Contract(pure = true)
+    private synchronized int getSaldoOgolne() {
         int saldoOg = 0;
-        bankLock.lock();
-        try {
-
-            for (int i = 0; i < LICZBA_KONT; i++) {
-                saldoOg += konta[i];
-
-            }
-        } finally {
-            bankLock.unlock();
+        for (int i = 0; i < LICZBA_KONT; i++) {
+            saldoOg += konta[i];
         }
         return saldoOg;
     }
 
-    private int getSaldo(int nr) {
-        bankLock.lock();
-        try {
-            return konta[nr];
-        } finally {
-            bankLock.unlock();
-        }
+    @Contract(pure = true)
+    private synchronized int getSaldo(int nr) {
+        return konta[nr];
+
     }
 }
--- src/pl/niewiel/PrzelewRunnable.java
+++ src/pl/niewiel/PrzelewRunnable.java
@@ -24,7 +24,6 @@ public class PrzelewRunnable implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
     }
 }
